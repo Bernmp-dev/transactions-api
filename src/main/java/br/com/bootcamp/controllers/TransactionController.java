d505c44acde0a893a4fed13a4c36c41b983553d9
bernmp-dev <bernardomp.dev@gmail.com>
2024-01-29 17:13:26 -0300

    [model]feat: insertTransaction, getTransactionById.

--- src/main/java/br/com/bootcamp/controllers/TransactionController.java
+++ src/main/java/br/com/bootcamp/controllers/TransactionController.java
@@ -4,6 +4,7 @@ import br.com.bootcamp.models.entities.TransactionEntity;
 import br.com.bootcamp.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,5 +21,18 @@ public class TransactionController {
     @ResponseStatus(HttpStatus.OK)
     public List<TransactionEntity> getAllTransactions() {
         return transactionService.getAllTransactions();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public TransactionEntity insertTransaction(
+            @RequestBody TransactionEntity transaction
+    ) {
+        return transactionService.insertTransaction(transaction);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getTransactionById(@PathVariable Long id) {
+        return transactionService.getTransactionById(id);
     }
 }
