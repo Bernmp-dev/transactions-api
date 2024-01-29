8ea897146a9e609da87abbf0a3d23ae58b8d885c
bernmp-dev <bernardomp.dev@gmail.com>
2024-01-29 15:23:54 -0300

    fix: TransactionService .

--- src/main/java/br/com/bootcamp/services/TransactionService.java
+++ src/main/java/br/com/bootcamp/services/TransactionService.java
@@ -2,9 +2,11 @@ package br.com.bootcamp.services;
 
 import br.com.bootcamp.models.entities.TransactionEntity;
 import br.com.bootcamp.models.repositories.TransactionRepo;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class TransactionService {
     private TransactionRepo transactionRepo;
 
