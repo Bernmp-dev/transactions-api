2f82a3807876ef9a559adb845764501282a55818
bernmp-dev <bernardomp.dev@gmail.com>
2024-01-29 17:11:59 -0300

    feat: TransactionEntity fields validation.

--- src/main/java/br/com/bootcamp/models/entities/TransactionEntity.java
+++ src/main/java/br/com/bootcamp/models/entities/TransactionEntity.java
@@ -1,6 +1,9 @@
 package br.com.bootcamp.models.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -15,8 +18,15 @@ public class TransactionEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank(message = "Category não pode ser nulo ou vazio!")
     private String category;
+
+    @NotNull(message = "Value não pode estar vazio!")
     private Double value;
+
+    @NotNull(message = "Data não pode ser nula!")
+    @PastOrPresent(message = "Data não pode ser no futuro!")
     private LocalDate date;
 
     public TransactionEntity() {
