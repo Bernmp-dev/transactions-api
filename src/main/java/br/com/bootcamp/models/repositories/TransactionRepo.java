da7347bef7b4e19c7eff57bba956b02e33f02557
bernmp-dev <bernardomp.dev@gmail.com>
2024-01-29 22:32:23 -0300

    [TransactionRepo]feat: findAllByDate, getAllCategorySum, sumValuesByCategory.

--- src/main/java/br/com/bootcamp/models/repositories/TransactionRepo.java
+++ src/main/java/br/com/bootcamp/models/repositories/TransactionRepo.java
@@ -1,9 +1,22 @@
 package br.com.bootcamp.models.repositories;
 
 import br.com.bootcamp.models.entities.TransactionEntity;
+import br.com.bootcamp.models.interfaces.CategorySumProjection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface TransactionRepo extends JpaRepository<TransactionEntity, Long> {
+
+    List<TransactionEntity> findAllByDate(LocalDate date);
+
+    @Query("SELECT te.category AS category, SUM(te.value) AS sum FROM TransactionEntity te GROUP BY te.category")
+    List<CategorySumProjection> getAllCategorySum();
+
+    @Query("SELECT te.category AS category, SUM(te.value) AS sum FROM TransactionEntity te WHERE te.category = :category GROUP BY te.category")
+    CategorySumProjection sumValuesByCategory(String category);
 }
