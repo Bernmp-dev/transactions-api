a16a788a06b419e1259aa476162aa79e5db92c10
bernmp-dev <bernardomp.dev@gmail.com>
2024-01-30 00:03:13 -0300

    feat: swagger config .

--- src/main/java/br/com/bootcamp/controllers/TransactionController.java
+++ src/main/java/br/com/bootcamp/controllers/TransactionController.java
@@ -15,7 +15,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@CrossOrigin("*")
+@CrossOrigin(origins = "*")
 @RequestMapping("/transaction")
 public class TransactionController {
 
